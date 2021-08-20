5adcde6f9b28d807217dd05345cb43125a7660bb
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-20 11:25:52 +0600

    added onClick function for buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
 function Button (props) {
+  const [active, setActive] = useState(false)
   return(
     <div className="button-wrapper">
-      <button className="button">
+      <button className={`button ${active ? "active" : ""}`} onClick={() => active ? setActive(false) : setActive(true)}>
         {props.icon}
       </button>
     </div>
