6355642449ebd0f3cc9d6f4e5340b84ad5825cc6
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-20 11:21:50 +0600

    added function for upload and render image to Image component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@ import UploadButton from './components/UploadButton';
 import Image from './components/Image';
 import Buttons from './components/Buttons';
 import './styles/App.css';
+import { useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState(null);
+
+  async function changeImage(image){
+    const base64 = await convertImageToBase64(image);
+    setImage(base64);
+  }
+
+  function convertImageToBase64(image){
+    if(image){
+      return new Promise((res, rej) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(image);
+
+        fileReader.onload = () => {
+          res(fileReader.result);
+        }
+
+        fileReader.onerror = (error) => {
+          rej(error);
+        }
+      });
+    }
+  }
+
   return (
     <div className="App container">
-      <UploadButton/>
-      <Image/>
+      <UploadButton changeImage={changeImage}/>
+      <Image image={image}/>
       <Buttons/>
     </div>
   );
--- src/components/UploadButton.jsx
+++ src/components/UploadButton.jsx
@@ -1,8 +1,13 @@
 function UploadButton (props) {
+  function changeImageHandler(event){
+    const image = event.target.files[0];
+    props.changeImage(image);
+  }
+
   return(
     <label className="label">
       Загрузить картинку
-      <input type="file" name="image" className="input" accept="image/*" />
+      <input type="file" name="image" className="input" accept="image/*" onChange={changeImageHandler} />
     </label>
   );
 }
